4bf9c9c7d662412c47af260aa6eab5f10762f72e
xjama <jamshidbekollanazarov@outlook.com>
2023-01-17 11:49:29 -0600

    o'zbek tiliga o'girildi

--- buttons.py
+++ buttons.py
@@ -2,12 +2,12 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from database import *
 
 buttons_close_keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-btn = KeyboardButton("Close")
+btn = KeyboardButton("Chiqish")
 buttons_close_keyboard.add(btn)
 
 def buttons_get_location_keyboard():
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    btn = KeyboardButton("Send location",request_location=True)
+    btn = KeyboardButton("Joylashuvni jo'natish",request_location=True)
     keyboard.add(btn)
     return keyboard
 
@@ -16,15 +16,15 @@ def buttons_get_restaurant_keyboard():
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     buttons = [KeyboardButton(r[0]) for r in restaurants]
     keyboard.add(*buttons)
-    btn = KeyboardButton("Close")
+    btn = KeyboardButton("Chiqish")
     keyboard.add(btn)
     return keyboard
 
 def buttons_get_menu_keyboard():
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    food_button = KeyboardButton("Order food")
-    table_button = KeyboardButton("Order table")
-    delivery_button = KeyboardButton("Order delivery")
+    food_button = KeyboardButton("Ovqat buyurtma")
+    table_button = KeyboardButton("Stul buyurtma")
+    delivery_button = KeyboardButton("Taom yetkazib berish")
     keyboard.add(food_button, table_button, delivery_button)
     return keyboard
 
@@ -33,22 +33,22 @@ def buttons_get_restaurant_menu_keyboard(restaurant_name):
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     buttons = [KeyboardButton(m[0]) for m in menu]
     keyboard.add(*buttons)
-    btn = KeyboardButton("Close")
+    btn = KeyboardButton("Chiqish")
     keyboard.add(btn)
     return keyboard
 
 def buttons_get_table_type_keyboard():
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    table_type_1 = KeyboardButton("1 person")
-    table_type_2 = KeyboardButton("2 persons")
-    table_type_3 = KeyboardButton("Family")
+    table_type_1 = KeyboardButton("1 odam")
+    table_type_2 = KeyboardButton("2 odamlar")
+    table_type_3 = KeyboardButton("Oilaviy")
     keyboard.add(table_type_1, table_type_2, table_type_3)
-    btn = KeyboardButton("Close")
+    btn = KeyboardButton("Chiqish")
     keyboard.add(btn)
     return keyboard
 
 def buttons_get_phone_number_keyboard():
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    phone_button = KeyboardButton("Share Phone Number", request_contact=True)
+    phone_button = KeyboardButton("Telefon raqamni jo'natish", request_contact=True)
     keyboard.add(phone_button)
     return keyboard
--- main.py
+++ main.py
@@ -17,68 +17,68 @@ dp = Dispatcher(bot, storage=storage)
 #START
 @dp.message_handler(commands='start')
 async def cmd_start(message: types.Message):
-    await bot.send_message(message.chat.id, "Welcome to the restaurant ordering bot! Please choose an option:", reply_markup=buttons_get_menu_keyboard())
+    await bot.send_message(message.chat.id, "Salom bizning mehmonhona bo'timizga hush kelibsiz. Kerakli bo'limni tanlashingiz mumkin:", reply_markup=buttons_get_menu_keyboard())
 
 #MAIN MENU
 @dp.message_handler()
 async def handle_text(message: types.Message):
-    if message.text.lower() == "Order food".lower():
-        await bot.send_message(message.chat.id, f"Please select a restaurant:",reply_markup=buttons_get_restaurant_keyboard())
+    if message.text.lower() == "Ovqat buyurtma".lower():
+        await bot.send_message(message.chat.id, f"Iltimos restaranni tanlang:",reply_markup=buttons_get_restaurant_keyboard())
         await states_order_food.restaurant_name.set()
-    elif message.text.lower() == "Order table".lower():
-        await bot.send_message(message.chat.id, "Please select a restaurant:",reply_markup=buttons_get_restaurant_keyboard())
+    elif message.text.lower() == "Stul buyurtma".lower():
+        await bot.send_message(message.chat.id, "Iltimos restaranni tanlang:",reply_markup=buttons_get_restaurant_keyboard())
         await states_order_table.restaurant_name.set()
-    elif message.text.lower() == "Order delivery".lower():
-        await bot.send_message(message.chat.id, "Please select a restaurant:", reply_markup=buttons_get_restaurant_keyboard())
+    elif message.text.lower() == "Taom yetkazib berish".lower():
+        await bot.send_message(message.chat.id, "Iltimos restaranni tanlang:", reply_markup=buttons_get_restaurant_keyboard())
         await states_order_delivery.restaurant_name.set()
 
 
 #ORDER FOOD
 @dp.message_handler(content_types=types.ContentType.TEXT,state=states_order_food.restaurant_name)
 async def f11(message:types.Message,state:FSMContext):
-    if message.text == "Close":
-        await message.answer("Closed:",reply_markup=buttons_get_menu_keyboard())
+    if message.text == "Chiqish":
+        await message.answer("Chiqildi:",reply_markup=buttons_get_menu_keyboard())
         await state.finish()
     else:
         restaurant_name = message.text
         if not database_check_restaurant_exist(restaurant_name):
-            await message.answer("Invalid restaurant. Please select a valid restaurant:",
+            await message.answer("Restaran nomi noto'g'ri. Iltimos restaranni tanlang:",
                                  reply_markup=buttons_get_restaurant_keyboard())
         else:
-            await message.answer("You selected {}. Please choose a menu::".format(restaurant_name),
+            await message.answer("Siz {} restaranini tanladingiz. Iltimos menyuni tanlang:".format(restaurant_name),
                                  reply_markup=buttons_get_restaurant_menu_keyboard(restaurant_name))
             await state.update_data(restaurant_name=restaurant_name)
             await states_order_food.menu.set()
 
 @dp.message_handler(content_types=types.ContentType.TEXT,state = states_order_food.menu)
 async def f10(message:types.Message,state:FSMContext):
-    if message.text == "Close":
-        await message.answer("Closed:",reply_markup=buttons_get_menu_keyboard())
+    if message.text == "Chiqish":
+        await message.answer("Chiqildi:",reply_markup=buttons_get_menu_keyboard())
         await state.finish()
     else:
         menu = message.text
         data = await state.get_data()
         restaurant_name = data.get("restaurant_name")
         if not database_check_menu_exist(restaurant_name, menu):
-            await message.answer("Invalid menu. Please choose a valid menu:",
+            await message.answer("Menyu noto'g'ri! Iltimos menyuni tanlang:",
                                  reply_markup=buttons_get_restaurant_menu_keyboard(restaurant_name))
         else:
-            await message.answer("You ordered {} from {} restaurant. Choose table type:".format(menu, restaurant_name),reply_markup=buttons_get_table_type_keyboard())
+            await message.answer("Siz {} menyusini {} restaranidagi tanladingiz. Iltimos stol turini tanlang:".format(menu, restaurant_name),reply_markup=buttons_get_table_type_keyboard())
             await state.update_data(menu=menu)
             await states_order_food.type_table.set()
 
 @dp.message_handler(content_types=types.ContentType.TEXT,state=states_order_food.type_table)
 async def f9(message:types.Message,state:FSMContext):
-    if message.text == "Close":
-        await message.answer("Closed:",reply_markup=buttons_get_menu_keyboard())
+    if message.text == "Chiqish":
+        await message.answer("Chiqildi:",reply_markup=buttons_get_menu_keyboard())
         await state.finish()
     else:
-        if message.text in ["1 person","2 persons","Family"]:
-            await message.answer("Please enter a phone number:",reply_markup=buttons_get_phone_number_keyboard())
+        if message.text in ["1 odam","2 odamlar","Oilaviy"]:
+            await message.answer("Iltimos telefon raqamingizni jo'nating:",reply_markup=buttons_get_phone_number_keyboard())
             await state.update_data(type_table = message.text)
             await states_order_food.phone_number.set()
         else:
-            await message.answer("Table type is incorrect!!!")
+            await message.answer("Stol turi noto'g'ri!!!")
 @dp.message_handler(content_types=types.ContentType.CONTACT,state=states_order_food.phone_number)
 async def f8(message:types.Message,state:FSMContext):
     data = await state.get_data()
@@ -88,23 +88,23 @@ async def f8(message:types.Message,state:FSMContext):
     phone_number = message.contact.phone_number
     full_name = message.contact.full_name
     user_id = message.contact.user_id
-    await bot.send_message(ADMIN_ID,f"Ordered food:\nRestaurant name: {restaurant_name}\nTable type: {table_type}\nFood: {menu}\nPhone number: {phone_number}\nFull name: {full_name}\nUser id: {user_id}")
-    await message.answer("Ordered food.",reply_markup=buttons_get_menu_keyboard())
+    await bot.send_message(ADMIN_ID,f"Ovqatga buyurtma:\nRestauran nomi: {restaurant_name}\nstol turi: {table_type}\novqat: {menu}\nTelefon raqami: {phone_number}\nTo'liq ism: {full_name}\nUser id: {user_id}")
+    await message.answer("Ovqat buyurtma qilindi.",reply_markup=buttons_get_menu_keyboard())
     await state.finish()
 
 #ORDER DEILIVERY
 @dp.message_handler(content_types=types.ContentType.TEXT,state=states_order_delivery.restaurant_name)
 async def f7(message:types.Message,state:FSMContext):
-    if message.text == "Close":
-        await message.answer("Closed:",reply_markup=buttons_get_menu_keyboard())
+    if message.text == "Chiqish":
+        await message.answer("Chiqildi:",reply_markup=buttons_get_menu_keyboard())
         await state.finish()
     else:
         restaurant_name = message.text
         if not database_check_restaurant_exist(restaurant_name):
-            await message.answer("Invalid restaurant. Please select a valid restaurant:",
+            await message.answer("Restaran nomi noto'g'ri iltimos restaran nomini tanlang:",
                                  reply_markup=buttons_get_restaurant_keyboard())
         else:
-            await message.answer("You selected {}. Please choose a menu:".format(restaurant_name),
+            await message.answer("Siz {} restaranini tanladingiz. Iltimos menyuni tanlang:".format(restaurant_name),
                                  reply_markup=buttons_get_restaurant_menu_keyboard(restaurant_name))
             await state.update_data(restaurant_name=restaurant_name)
             await states_order_delivery.menu.set()
@@ -115,16 +115,16 @@ async def f6(message:types.Message,state:FSMContext):
     data = await state.get_data()
     restaurant_name = data.get("restaurant_name")
     if not database_check_menu_exist(restaurant_name, menu):
-        await message.answer("Invalid menu. Please choose a valid menu:",
+        await message.answer("Menyu xato! Iltimos menyuni tanlang:",
                              reply_markup=buttons_get_restaurant_menu_keyboard(restaurant_name))
     else:
-        await message.answer("You ordered {} from {} restaurant. Please send location:".format(menu,restaurant_name),reply_markup=buttons_get_location_keyboard())
+        await message.answer("Siz {} menyusini {} restarandagi tanladingiz. Iltimos joylashuvingizni jo'nating:".format(menu,restaurant_name),reply_markup=buttons_get_location_keyboard())
         await state.update_data(menu=menu)
         await states_order_delivery.location.set()
 
 @dp.message_handler(content_types=types.ContentType.LOCATION,state=states_order_delivery.location)
 async def f5(message:types.Message,state:FSMContext):
-    await message.answer("Please enter a phone number:",reply_markup=buttons_get_phone_number_keyboard())
+    await message.answer("Iltimos telefon raqamingizni jo'nating:",reply_markup=buttons_get_phone_number_keyboard())
     await state.update_data(location = message.location)
     await states_order_delivery.phone_number.set()
 
@@ -137,40 +137,40 @@ async def f4(message:types.Message,state:FSMContext):
     phone_number = message.contact.phone_number
     full_name = message.contact.full_name
     user_id = message.contact.user_id
-    await bot.send_message(ADMIN_ID,f"Ordered delivery:\nRestaurant name: {restaurant_name}\nFood: {menu}\nPhone number: {phone_number}\nFull name: {full_name}\nUser id: {user_id}")
+    await bot.send_message(ADMIN_ID,f"Ovqat yetkazib berish:\nRestaurant name: {restaurant_name}\nOvqat: {menu}\nTelefon raqam: {phone_number}\nTo'liq ism: {full_name}\nUser id: {user_id}")
     await bot.send_location(ADMIN_ID,latitude=location['latitude'],longitude=location['longitude'])
-    await message.answer("Ordered delivery.",reply_markup=buttons_get_menu_keyboard())
+    await message.answer("Ovqat yetkazib berishga buyurtma berildi.",reply_markup=buttons_get_menu_keyboard())
     await state.finish()
 
 #ORDER TABLE
 @dp.message_handler(content_types=types.ContentType.TEXT,state=states_order_table.restaurant_name)
 async def f3(message:types.Message,state:FSMContext):
-    if message.text == "Close":
-        await message.answer("Closed:",reply_markup=buttons_get_menu_keyboard())
+    if message.text == "Chiqish":
+        await message.answer("Chiqildi:",reply_markup=buttons_get_menu_keyboard())
         await state.finish()
     else:
         restaurant_name = message.text
         if not database_check_restaurant_exist(restaurant_name):
-            await message.answer("Invalid restaurant. Please select a valid restaurant:",
+            await message.answer("Restaran nomi noto'g'ri! Iltimos restaran nomini tanlang:",
                                  reply_markup=buttons_get_restaurant_keyboard())
         else:
-            await message.answer("You selected {}. Please choose a table type::".format(restaurant_name),
+            await message.answer("Siz {} restaranini tanladingiz Iltimos stol turini tanlang. :".format(restaurant_name),
                                  reply_markup=buttons_get_table_type_keyboard())
             await state.update_data(restaurant_name=restaurant_name)
             await states_order_table.type_table.set()
 
 @dp.message_handler(content_types=types.ContentType.TEXT,state=states_order_table.type_table)
 async def f2(message:types.Message,state:FSMContext):
-    if message.text == "Close":
-        await message.answer("Closed:",reply_markup=buttons_get_menu_keyboard())
+    if message.text == "Chiqish":
+        await message.answer("Chiqildi:",reply_markup=buttons_get_menu_keyboard())
         await state.finish()
     else:
-        if message.text in ["1 person","2 persons","Family"]:
-            await message.answer("Please enter a phone number:",reply_markup=buttons_get_phone_number_keyboard())
+        if message.text in ["1 odam","2 odamlar","Oilaviy"]:
+            await message.answer("Iltimos telefon raqamingizni jo'nating:",reply_markup=buttons_get_phone_number_keyboard())
             await state.update_data(type_table = message.text)
             await states_order_table.phone_number.set()
         else:
-            await message.answer("Table type is incorrect!!!")
+            await message.answer("Stol turi noto'g'ri!!!")
 @dp.message_handler(content_types=types.ContentType.CONTACT,state=states_order_table.phone_number)
 async def f1(message:types.Message,state:FSMContext):
     data = await state.get_data()
@@ -179,8 +179,8 @@ async def f1(message:types.Message,state:FSMContext):
     phone_number = message.contact.phone_number
     full_name = message.contact.full_name
     user_id = message.contact.user_id
-    await bot.send_message(ADMIN_ID,f"Ordered table:\nRestaurant name: {restaurant_name}\nTable type: {table_type}\nPhone number: {phone_number}\nFull name: {full_name}\nUser id: {user_id}")
-    await message.answer("Ordered table.",reply_markup=buttons_get_menu_keyboard())
+    await bot.send_message(ADMIN_ID,f"Stolga buyurtma:\nRestaurant name: {restaurant_name}\nStol turi: {table_type}\nTelefon raqam: {phone_number}\nTo'liq ism: {full_name}\nUser id: {user_id}")
+    await message.answer("Stol buyurtma qilindi.",reply_markup=buttons_get_menu_keyboard())
     await state.finish()
 
 
